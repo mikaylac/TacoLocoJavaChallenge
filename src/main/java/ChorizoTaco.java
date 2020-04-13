431e4a71f10e721b9d069e58b9e7a42a96bbd608
Mikayla Clemons <mjclemons3@gmail.com>
2020-04-12 20:04:53 -0400

    002 Wrote test to verify a chorizo taco can be added to taco order.

---
+++ src/main/java/ChorizoTaco.java
@@ -0,0 +1,2 @@
+public class ChorizoTaco implements Taco {
+}
--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 public class OrderTest {
 
     @Test
-    public void shouldAddVeggieTacoToOrder() {
+    public void shouldAddVeggieTacoToOrder_WhenAddToOrderCalled() {
         Order testTacoOrder = new Order();
         VeggieTaco veggieTaco = new VeggieTaco();
 
@@ -17,4 +17,18 @@ public class OrderTest {
         assertEquals(expectedNumberOfTacosAdded, actualNumberOfTacosAdded);
         assertTrue(testTacoOrder.completeOrder.containsKey(veggieTaco));
     }
+
+    @Test
+    public void shouldAddChorizoTacoToOrder_WhenAddToOrderCalled(){
+        Order testTacoOrder = new Order();
+        ChorizoTaco chorizoTaco = new ChorizoTaco();
+
+        testTacoOrder.addToOrder(chorizoTaco, 1);
+        int expectedNumberOfTacosAdded = 1;
+        int actualNumberOfTacosAdded = testTacoOrder.completeOrder.get(chorizoTaco);
+
+        assertEquals(expectedNumberOfTacosAdded, actualNumberOfTacosAdded);
+        assertTrue(testTacoOrder.completeOrder.containsKey(chorizoTaco));
+    }
+    
 }
